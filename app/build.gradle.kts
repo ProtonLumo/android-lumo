68220c95cc9ad26c5e969afb17cc2a8702c4d9f1
vlad <vlad.cipariu@proton.ch>
2025-10-22 17:29:07 +0100

    removes Billing permission from nogms build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
     defaultConfig {
         minSdk = 29
         targetSdk = 35
-        versionCode = 35
-        versionName = "1.2.2"
+        versionCode = 36
+        versionName = "1.2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
